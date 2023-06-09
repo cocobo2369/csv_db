92fa183d83a575721abe1cc798115c69e4862a68
CBInovator <cocobo2369@gmail.com>
2023-06-10 03:20:46 +0900

    df_sub_B 의 db 추가

--- manage_db.py
+++ manage_db.py
@@ -12,6 +12,11 @@ def make_dataframe() :
     df_sub_A_concat=pd.concat([df_sub_A_1,df_sub_A_2])
     df_merge=pd.merge(df_main,df_sub_A_concat,on="sub")
 
+    df_sub_B_1=pd.read_csv(build_path+"db_sub_B_1.csv")
+    df_sub_B_2=pd.read_csv(build_path+"db_sub_B_2.csv")
+    df_sub_B_3=pd.read_csv(build_path+"db_sub_B_3.csv")
+    df_sub_B_concat=pd.concat([df_sub_B_1,df_sub_B_2,df_sub_B_3])
+    df_merge=pd.merge(df_merge,df_sub_B_concat,on="sub2")
     return df_merge
 
 app = Flask(__name__)
