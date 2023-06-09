a53a4b2e80ca1c059a645fe103f2ed161032da49
CBInovator <cocobo2369@gmail.com>
2023-06-10 03:07:58 +0900

    html로 csv data 표현

--- manage_db.py
+++ manage_db.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
+import os
+import pandas as pd
+
+build_path=os.path.dirname(os.path.abspath(__file__))+"\\database\\"
+def make_dataframe() :
+
+    df_main=pd.read_csv(build_path+"db_main.csv")
+
+    return df_main
 
 app = Flask(__name__)
 
 @app.route('/')
 def display_dataframe():
-
-    return render_template('table.html')
+    df=make_dataframe() 
+    return render_template('table.html',table=df.to_html())
 
 if __name__ == '__main__' :
     app.run(host='127.0.0.1', port=5000)
